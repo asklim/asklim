2ad51d235736bf856399bf1535be858c961420a5
Andrei Klim <asklim@outlook.com>
2019-12-18 00:43:06 +0300

    Created, version 0.0.1

---
+++ __tests__/week21c.spec.js
@@ -0,0 +1,56 @@
+
+const { week21c } = require('..');
+const { 
+  datesAsString,
+  datesAsDate,
+  datesSet,
+} = require('./dataset-helper');
+
+
+describe("function week21c() testing ...", () => {
+  
+  const dtNow = new Date();
+  //let dtNow = Date.now();
+  let strNow = dtNow.toString();
+
+  test('week21c( Date ) should be return Number', () => {
+    let value = week21c( dtNow );
+    expect( typeof( value )).toBe( 'number' );
+  });
+
+  test('week21c( String )should be return Number', () => {
+    let value = week21c(strNow);
+    expect( typeof( value )).toBe( 'number' );
+  });
+
+  
+  datesAsString( datesSet )
+  .forEach( elem => {
+    test(`${elem[0]} is ${elem[1]} week.`, () => {
+      expect( week21c( elem[0] )).toBe( elem[1] );
+    });  
+  });
+
+  datesAsDate( datesSet )
+  .forEach( elem => {
+    test(`${elem[0]} is ${elem[1]} week.`, () => {
+      expect( week21c( elem[0] )).toBe( elem[1] );
+    });  
+  });
+
+/*  
+  console.log(new Date(-4713,10,23));
+  test('First Julian day (JDN=1) Nov,23 -4713 is week -350271', () => {
+    expect( week21c( new Date(-4713,10,23)) ).toBe( -350271 );
+    // receive -350273
+  });
+*/
+
+  it('42 equal 42', () => {
+    expect("42").toBe("42");
+  });
+
+  it('2 equal 2', () => {
+    expect("2").toBe("2");
+  });
+});
---
+++ utils.js
@@ -0,0 +1,8 @@
+
+var { julianDay } = require('./lib/week21c/julian-day');
+var { week21c } = require('./lib/week21c/week21c');
+
+module.exports = {
+  julianDay,
+  week21c 
+};
