409fbaf68b7b61340ac8d86f72541c279f759382
Andrei Klim <asklim@outlook.com>
2021-04-13 14:50:14 +0300

    0.0.3 added lib/http, lib/logger

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./weeks');
+const weeks = require( './weeks' );
+const httpResponseCodes = require( './lib/http/http-response-codes' );
+const httpResponses = require( './lib/http/http-responses' );
+const consoleLogger = require( './lib/logger/logger' );
+
+module.exports = {
+    ... weeks,
+    ... httpResponseCodes,
+    ... httpResponses,
+    consoleLogger,
+};
